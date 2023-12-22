8d95600b0eac7de088189d3a626e38271033678a
ahsanhabib <ahsanhabib72829@gmail.com>
2023-12-22 16:22:22 +0600

    fast-commit-tha-4project

---
+++ project-1.py
@@ -0,0 +1,11 @@
+answer = input("do you want to play this game? [yes/no]: ")
+
+if answer == "yes":
+                print("welcome to the game! " )
+                answer  = input("do you want to go jungle or canve?[jungle /canve]")
+                if answer == "jungle":
+                    print (" you see the hungry tiger ,togerr wil ear you.  ")
+                elif answer == "canve" :
+                    print ("you seen the bear in front of cabe")
+                
+            
