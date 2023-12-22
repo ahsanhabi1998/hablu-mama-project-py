c519b0ccfd7d927d100c00fa9ddbd64690c66edb
ahsanhabib <ahsanhabib72829@gmail.com>
2023-12-22 16:43:38 +0600

    2end-commit

--- project-1.py
+++ project-1.py
@@ -1,11 +1,22 @@
 answer = input("do you want to play this game? [yes/no]: ")
 
 if answer == "yes":
+
                 print("welcome to the game! " )
                 answer  = input("do you want to go jungle or canve?[jungle /canve]")
                 if answer == "jungle":
                     print (" you see the hungry tiger ,togerr wil ear you.  ")
                 elif answer == "canve" :
                     print ("you seen the bear in front of cabe")
-                
-            
+                    answer = input ("do you want to fight wit the bear or run away! : ")
+                    if answer == "fight":
+                        print ("bear is too much strong! you loss the game!")
+                    else:
+                        print("wow! still you are alive!") 
+else:
+    print("the game closed")
+                    
+                    
+                        
+                    
+                    
